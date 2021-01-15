7a191b20e215450dda8615f0d69c40761be2da9d
Benjamin Hargett <hargettbenjamin@gmail.com>
2021-01-14 20:29:20 -0500

    add heroicon to assets precompile

--- lib/heroicon/engine.rb
+++ lib/heroicon/engine.rb
@@ -3,5 +3,9 @@
 module Heroicon
   class Engine < ::Rails::Engine
     isolate_namespace Heroicon
+
+    initializer "heroicon.precompile_assets" do |app|
+      app.config.assets.precompile << "heroicon_manifest.js"
+    end
   end
 end
