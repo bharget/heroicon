ac4dddef5453e694a59e54e83deb2bcacd57e437
Matthew Nguyen <contact@n-studio.fr>
2022-08-28 20:36:01 +0800

    doc

--- README.md
+++ README.md
@@ -87,7 +87,7 @@ An example configuration looks like this:
 ```ruby
 Heroicon.configure do |config|
   config.variant = :solid
-  config.default_class = {solid: "h-5 w-5", outline: "h-6 w-6"}
+  config.default_class = {solid: "h-5 w-5", outline: "h-6 w-6", mini: "h-4 w-4"}
 end
 ```
 
--- lib/heroicon/icon.rb
+++ lib/heroicon/icon.rb
@@ -41,7 +41,7 @@ module Heroicon
     #
     # @example
     #   Heroicon.configure do |config|
-    #     config.default_class = { solid: "h-5 w-5", outline: "h-6 w-6" }
+    #     config.default_class = { solid: "h-5 w-5", outline: "h-6 w-6", mini: "h-4 w-4" }
     #   end
     #
     #   #=> <svg class="h-5 w-5">...</svg>
